c1cd9660d50533ab06df4bdcfb4072c2059fce65
Tanmoy-M17 <97627356+Tanmoy-M17@users.noreply.github.com>
2022-05-02 17:09:06 +0530

    done

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -1,3 +1,9 @@
-// action types
-
+export const LOGIN_TOGGLE = "LOGIN_TOGGLE";
 // Action Creators
+
+export const login = (data) => {
+   return {
+      type: LOGIN_TOGGLE,
+      payload: data,
+   };
+};
--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,8 +1,17 @@
-const init = {};
+import { LOGIN_TOGGLE } from "./actions";
 
-export const reducer = (store = init, { type, payload }) => {
-  switch (type) {
-    default:
-      return store;
-  }
+// const init = {
+//    role: false,
+// };
+
+export const reducer = (store, { type, payload }) => {
+   switch (type) {
+      case LOGIN_TOGGLE:
+         return {
+            ...store,
+            login: payload.login,
+         };
+      default:
+         return store;
+   }
 };
